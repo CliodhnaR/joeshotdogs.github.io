d378cf56fc5846f789728a15323523664ae24a03
CliodhnaR <113465549+CliodhnaR@users.noreply.github.com>
2022-10-05 15:47:33 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,60 @@
+// associative array
+const items = [];
+items["hot dogs"] = 0;
+items["french fries"] = 0;
+items["sodas"] = 0;
+
+for(i in items){
+	items[i] = prompt("How many " + i + " would you like?");
+	items[i] = parseInt(items[i]);
+	// if what was entered is not an integer
+	while (!(Number.isInteger(items[i]))){
+		alert("Sorry, please enter an integer!");
+		items[i] = prompt("How many " + i + " would you like?");
+		items[i] = parseInt(items[i]);
+	}
+}
+
+let hdogs = items["hot dogs"];
+let fries = items["french fries"];
+let sodas = items["sodas"];
+
+document.getElementById("orders").innerHTML = "Hot dogs: " + hdogs + "<br />" + " French fries: " + fries + "<br />" + " Sodas: " + sodas;
+
+let subtotal = hdogs*4.25 + fries*2.60 + sodas*1.50;
+
+document.getElementById("subtotal").innerHTML = "Subtotal: $" + addZero(round(subtotal));
+
+let discount = 0;
+
+if(subtotal >= 20) {
+	discount = subtotal * 0.10;
+	subtotal -= discount;
+	document.getElementById("discount").innerHTML = "Discount: $" + addZero(round(discount));
+}
+
+
+let tax = subtotal * 0.0625;
+subtotal += tax;
+
+document.getElementById("tax").innerHTML = "Tax: $" + addZero(round(tax));
+
+document.getElementById("total").innerHTML = "Final total: $" + addZero(round(subtotal));
+
+function round(num){
+	num = num * 100;
+	num = Math.round(num);
+	num = num/100;
+	return num;
+}
+
+function addZero(num){
+	alert("hello");
+	tempNum = (num*100)%10
+	alert("Num: " + num + " TempNum: " + tempNum);
+	if(tempNum < 1) {
+		alert("goodbye");
+		num = num + "0";
+	}
+	return num;
+}
